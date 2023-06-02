97d6a1b0f7eac56b3433f0cd3d3c6e947dec6992
Storm Weiner <stormweiner@gmail.com>
2023-06-02 11:48:09 -0700

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='atom'
-        version='0.1.0'
+        name='atomic_py',
+        version='0.1.0',
         packages=find_packages()
     )
+
+
+# Actiavte conda env
+# cd into dir
+# pip install -e .
